140cb9983edb16a1fc4ff188e92efa96be241bd3
Jay Chen <issuesattack@gmail.com>
2024-12-04 08:26:11 +0000

    add equality check and panic for unequal values in main function

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 )
 
 func main() {
+
+	a := 1
+	b := 2
+	if a != b {
+		panic("a is not equal to b")
+	}
+
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
